d0ce0fd9aaa9310d9e650e25a4cf88b963480267
kamran6433 <kamran_12@icloud.com>
2022-12-06 15:33:17 +0000

    Learning components in Vue.js.

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-Vue.component('component' {
-    data() {
-        return {
-            
-        }
-    }
+Vue.component('add-component', {
+    template: '<li>This is a component called by add-component in the HTML. I am reusable.</li>'
 });
 
 
@@ -16,7 +12,8 @@ var app = new Vue({
             users: [
                 {username: 'Kamran'},
                 {username: 'Dayyan'},
-                {username: 'Haider'}
+                {username: 'Haider'},
+                {username: 'Travis'}
             ]
         }
     },
@@ -31,4 +28,30 @@ var app = new Vue({
             console.log(this.username);
         }
     }
+});
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        seen: false
+    }
+});
+
+var app4 = new Vue({
+    el: '#app4',
+    data: {
+        message: 'Click below'
+    },
+    methods: {
+        reverseMessage() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    } 
 });
